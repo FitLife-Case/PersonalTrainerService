27febcc1933d10c86ef48a7d167d5fd915f3cfc5
Lukas Bæk-Nielsen <eaa24luba@students.eaaa.dk>
2026-05-21 10:31:08 +0200

    css

--- obj/Debug/net10.0/PersonalTrainerService.AssemblyInfo.cs
+++ obj/Debug/net10.0/PersonalTrainerService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalTrainerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c9112d4ce67d7265c5d2d4767f9cee3a881d137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69067ef4896f831e5a74d5f869c6caadfd60ea6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalTrainerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalTrainerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
